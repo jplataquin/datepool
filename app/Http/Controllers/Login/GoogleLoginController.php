ee227f838f52ef510dcceeb153d1d07280ceba4d
JP Lataquin <jp.lataquin@gmail.com>
2025-03-24 00:18:21 +0800

    fix

--- app/Http/Controllers/Login/GoogleLoginController.php
+++ app/Http/Controllers/Login/GoogleLoginController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
+use App\Http\Controllers\Controller;
 
 class GoogleLoginController extends Controller
 {
