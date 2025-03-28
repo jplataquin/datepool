07920c3e43ddefdce67cd560a85b85809c48db29
JP Lataquin <jp.lataquin@gmail.com>
2025-03-28 21:25:57 +0800

    fix

--- app/Http/Controllers/PaymongoTestController.php
+++ app/Http/Controllers/PaymongoTestController.php
@@ -22,13 +22,13 @@ class PaymongoTestController extends Controller
                         [
                             'amount'        => 1000,//10.00
                             'currency'      => 'PHP',
-                            'named'         => 'Toys and Bagoong',
+                            'name'         => 'Toys and Bagoong',
                             'quantity'      => 10
                         ],
                         [
                             'amount'        => 10050,//100.50
                             'currency'      => 'PHP',
-                            'named'         => 'Guns and Pancakes',
+                            'name'         => 'Guns and Pancakes',
                             'quantity'      => 2
                         ]
                         ],
@@ -50,7 +50,40 @@ class PaymongoTestController extends Controller
             'Content-Type'  => 'application/json',
             'Authorization' => 'Basic '.base64_encode( $secret_key.':' )
         ])->post('https://api.paymongo.com/v1/checkout_sessions');
-
-        print_r($response);
+        
+        echo son_encode([
+            'data' =>[
+                'attributes'=>[
+                    'cancel_url' => url('/'),
+                    'line_items' =>[
+                        [
+                            'amount'        => 1000,//10.00
+                            'currency'      => 'PHP',
+                            'name'         => 'Toys and Bagoong',
+                            'quantity'      => 10
+                        ],
+                        [
+                            'amount'        => 10050,//100.50
+                            'currency'      => 'PHP',
+                            'name'         => 'Guns and Pancakes',
+                            'quantity'      => 2
+                        ]
+                        ],
+                        'payment_method_types' =>[
+                            'card',
+                            'gcash',
+                            'paymaya',
+                            'grab_pay',
+                            'brankas_bdo',
+                            'brankas_metrobank'
+                        ],
+                        'reference_number' => 'POP10',
+                        'statement_descriptor' => 'Beam Gifts',
+                        'success_url' => url('/checkout/success')
+                ]
+            ]
+        ]);
+        echo "<br/><br/>";
+        print_r($response,true);
     }
 }
