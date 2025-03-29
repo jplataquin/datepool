c301455e1116e9fc772ea382d009f761f8620b1f
JP Lataquin <jp.lataquin@gmail.com>
2025-03-29 17:58:45 +0800

    fix

--- routes/paymongo.php
+++ routes/paymongo.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/primary/callback', [App\Http\Controllers\PaymongoTestController::class, 'callback']);
+Route::post('/test', [App\Http\Controllers\PaymongoTestController::class, 'test']);
 
 Route::post('/secondary/webhook/{id}', function () {
     return 'Hello World';
